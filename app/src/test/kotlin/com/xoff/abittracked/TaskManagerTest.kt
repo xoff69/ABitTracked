bdcfa8f7c6c7901c023579e28131418c0b676967
xoff69 <xoff.pichon@gmail.com>
2023-12-21 14:29:10 -0700

    proto contient le json

--- app/src/test/kotlin/com/xoff/abittracked/TaskManagerTest.kt
+++ app/src/test/kotlin/com/xoff/abittracked/TaskManagerTest.kt
@@ -2,11 +2,12 @@ package com.xoff.abittracked
 
 import com.google.gson.Gson
 import com.xoff.abittracked.model.TaskItem
+import com.xoff.abittracked.model.Tasks
 import junit.framework.TestCase.assertEquals
 import org.junit.Test
 class TaskManagerTestKt {
     @Test
-    fun serializeObjectListTest() {
+    fun serializeTasksListTest() {
         val tasks = listOf(
             TaskItem(1, "Technical Author"),
             TaskItem(2, "Technical Author"),
@@ -16,6 +17,21 @@ class TaskManagerTestKt {
 
         val json =
             """[{"id":1,"name":"Technical Author"},{"id":2,"name":"Technical Author"},{"id":3,"name":"Technical Editor"}]"""
+        assertEquals(serialized, json)
+    }
+    @Test
+    fun serialiseTasks() {
+        val tasks = listOf(
+            TaskItem(1, "Technical Author"),
+            TaskItem(2, "Technical Author"),
+            TaskItem(3, "Technical Editor")
+        )
+        val allTasks=Tasks("toto",tasks)
+
+        val serialized = Gson().toJson(allTasks)
+
+        val json =
+            """{"title":"toto","tasks":[{"id":1,"name":"Technical Author"},{"id":2,"name":"Technical Author"},{"id":3,"name":"Technical Editor"}]}"""
         assertEquals(serialized, json)
     }
 }
